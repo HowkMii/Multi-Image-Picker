5c4106c2c8ce67ecdce4eabdbd3eb8ddf4077848
howkmii <abdelhakim@univ-tiaret.dz>
2021-09-29 20:33:39 +0100

    selectCircleStrokeColor

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,8 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedAssets:images,
         enableCamera: true, 
         materialOptions: MaterialOptions(
-          actionBarColor: 'blue'
+          actionBarColor: 'green',
+          selectCircleStrokeColor: 'green'
         )
       );
       setState(() {

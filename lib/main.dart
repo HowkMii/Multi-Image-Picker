6e6761880cc9f63a5eb16e05a5b8b40615d8f86f
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2021-09-30 21:41:06 +0100

    small modification

--- lib/main.dart
+++ lib/main.dart
@@ -2,79 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:multi_image_picker2/multi_image_picker2.dart';
 
 void main() {
-  runApp(MyApp());
+ runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+ // This widget is the root of your application.
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     title: 'Flutter Demo',
+     theme: ThemeData(
+       primarySwatch: Colors.blue,
+     ),
+     home: MyHomePage(title: 'Flutter Demo Home Page'),
+   );
+ }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+ MyHomePage({Key key, this.title}) : super(key: key);
+ final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+ @override
+ _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Asset> images =List<Asset>();
-  Future loadAssets() async{
-    List<Asset> resultList =List<Asset>();
-    try{
-      resultList =await MultiImagePicker.pickImages(
-        maxImages: 300,
-        selectedAssets:images,
-        enableCamera: true, 
-        materialOptions: MaterialOptions(
-          actionBarColor: 'green',
-          selectCircleStrokeColor: 'green'
-        )
-      );
-      setState(() {
-              images = resultList;
-            });
-    }catch(e){
-      print(e);
+ List<Asset> images =List<Asset>();
+ Future loadAssets() async{
+   List<Asset> resultList =List<Asset>();
+   try{
+     resultList =await MultiImagePicker.pickImages(
+       maxImages: 300,
+       selectedAssets:images,
+       enableCamera: true,
+       materialOptions: MaterialOptions(
+         actionBarColor: 'green',
+         selectCircleStrokeColor: 'green'
+       )
+     );
+     setState(() {
+             images = resultList;
+           });
+   }catch(e){
+     print(e);
 
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Pick Images"),
-        actions: [
-          FlatButton(
-            onPressed: loadAssets, 
-            child: Text("Pick image"),
-          ),
-        ],
-        
-      ),
-      body: GridView.count(
-        crossAxisCount: 3,
-        children: List.generate(images.length, (index){
-          return AssetThumb(
-            asset: images[index],
-            width: 300,
-            height: 300,
+   }
+ }
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text("Pick Images"),
+       actions: [
+         FlatButton(
+           onPressed: loadAssets,
+           child: Text("Pick image"),
+         ),
+       ],
+       
+     ),
+     body: GridView.count(
+       crossAxisCount: 3,
+       children: List.generate(images.length, (index){
+         return AssetThumb(
+           asset: images[index],
+           width: 300,
+           height: 300,
 
-          );
-        }),
-         // This trailing comma makes auto-formatting nicer for build methods.
-      ),
-    );
-  }
+         );
+       }),
+        // This trailing comma makes auto-formatting nicer for build methods.
+     ),
+   );
+ }
 }
